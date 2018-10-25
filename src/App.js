497bcbd21fe900be20bf8acfe77168e71faab00b
Sutton Ashby <korim_fidelis@yahoo.com>
2018-10-25 16:52:23 -0400

    checking out the tictactoe tutorial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Client} from 'boardgame.io/react';
+import {Game} from 'boardgame.io/core';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+function IsVictory(cells){
+  ... 
 }
+
+function isDraw(cells){
+  return cells.filter(c => c === null).length == 0;
+}
+
+
+const TicTacToe = Game({
+  setup: () => ({cells: Array(9).fill(null) }),
+
+  moves: {
+    clickCell(G, ctx, id) {
+      const cells = [...G.cells];
+      if(cells[id] === null){
+        cells[id] = ctx.currentPlayer;
+      }      
+      return {...G.cells}
+    },
+  },
+  flow: {
+    endGameIf:(G, ctx) => {
+      if(IsVictory(G.cells)) {
+        return {winner: ctx.currentPlayer };
+      }
+      if(isDraw(G.cells)) {
+        return {draw: true };
+      }
+    }
+  }
+});
+
+class TicTacToeBoard extends React.Component {
+  onClick(id){
+    if(this.isActive(id)){
+      this.props.moves.clickCell(id);
+      this.props.events.endTurn();
+    }
+  }
+
+  isActive(id){
+    if(!this.props.isActive) return false;
+    if(this.props.G.cells[id] !==null) return false;
+    return true;
+  }
+
+  render() {
+    let winner = '';
+    if(this.props.ctx.gameover){
+      winner =
+    }
+  }
+
+}
+
+
+const App = Client({game: TicTacToe });
 
 export default App;
